452919004deeb8c4be5a52852bea088f0771f923
OmarTamer55 <45062049+OmarTamer55@users.noreply.github.com>
2018-11-15 10:47:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,15 @@ client.on('ready', () => {
 
 
 
-// CODES HERE
+client.on('ready', async() => {
+var server = "512532251850833934"; 
+var channel = "512533817656082453
+
+";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('Welcome welcome')
+    },305);
+})6
 
 
 
